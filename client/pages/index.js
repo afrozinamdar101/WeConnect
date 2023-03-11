fec10c59ffe8f020c495e020975d8fb5cad74799
NeelakantaBhagavatula <neelakanta.bhagavatula@gmail.com>
2023-03-11 14:44:17 -0800

    Context creation to hold user data across app

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,10 @@
+import { useContext } from "react";
+
+import { UserContext } from "../context";
+
 const Home = () => {
+  const [state, setState] = useContext(UserContext);
+
   return (
     <div className="container">
       <div className="row">
