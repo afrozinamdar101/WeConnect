af52935649d8753bf17afdae2ad0e9d72028a18c
NeelakantaBhagavatula <neelakanta.bhagavatula@gmail.com>
2023-03-11 00:14:14 -0800

    Defined model, router, controller, hash password for registering

---
+++ server/controllers/auth.js
@@ -0,0 +1,30 @@
+import User from "../models/user.js";
+import { hashPassword, comparePassword } from "../helpers/auth.js";
+
+export const register = async (req, res) => {
+    // console.log("Register Endpoint => ", req.body);
+    const {name, email, password, secret } = req.body;
+
+    // validation
+    if (!name) return res.status(400).send("Name is required");
+    if (!password || password.length < 6) return res.status(400).send("Password is required and should be at least 6 characters long");
+    if (!secret) return res.status(400).send("Answer is required");
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) return res.status(400).send("Email is taken");
+
+    // hash password
+    const hashedPassword = await hashPassword(password);
+
+    const user = new User({name, email, password: hashedPassword, secret});
+
+    try {
+        await user.save();
+        console.log("Registered user =>", user);
+        return res.json({ ok: true });
+    } catch (err) {
+        console.log("Registration failed =>", err);
+        res.status(400).send("Error. Try again.");
+    }
+};
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.js";
 
 const app = express();
 
@@ -14,7 +15,7 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("DB Connected"))
+  .then(() => console.log("Connected to DB"))
   .catch((err) => console.log("DB connection error => ", err));
 
 //middleware
@@ -26,9 +27,9 @@ app.use(
   })
 );
 
-// //autoload routes
-
+// autoload routes
 // readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
+app.use("/api", authRoutes);
 
 app.post("/api/register", (req, res) => {
   console.log("Register Endpoint =>", req.body);
