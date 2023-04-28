23303903075f0b5fd8d7de823bcb320bba8309cd
NeelakantaBhagavatula <neelakanta.bhagavatula@gmail.com>
2023-04-28 16:23:55 -0700

    Create Post Form

---
+++ client/components/forms/CreatePostForm.js
@@ -0,0 +1,22 @@
+import { Avatar } from "antd";
+
+const CreatePostForm = () => {
+  return (
+    <div className="card">
+      <div className="card-body pb-3">
+        <form className="form-group">
+          <textarea
+            className="form-control"
+            placeholder="Write something..."
+          ></textarea>
+        </form>
+      </div>
+
+      <div className="card-footer">
+        <button className="btn btn-primary btn-sm mt-1">Post</button>
+      </div>
+    </div>
+  );
+};
+
+export default CreatePostForm;
--- client/pages/user/dashboard.js
+++ client/pages/user/dashboard.js
@@ -2,17 +2,25 @@ import { useContext } from "react";
 
 import { UserContext } from "../../context";
 import UserRoute from "../../components/routes/UserRoute";
+import CreatePostForm from "../../components/forms/CreatePostForm";
 
 const Dashboard = () => {
   const [state, setState] = useContext(UserContext);
 
   return (
     <UserRoute>
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <h1 className="display-1 text-center">Dashboard</h1>
+      <div className="container-fluid">
+        <div className="row py-5 text-light bg-default-image">
+          <div className="col text-center">
+            <h1>News Feed</h1>
           </div>
+        </div>
+
+        <div className="row py-5">
+          <div className="col md-8">
+            <CreatePostForm />
+          </div>
+          <div className="col md-4">Side bar</div>
         </div>
       </div>
     </UserRoute>
