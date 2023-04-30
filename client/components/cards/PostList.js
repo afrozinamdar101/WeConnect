a6e312ee8134a248c3907394b2b8f14a0739d29c
NeelakantaBhagavatula <neelakanta.bhagavatula@gmail.com>
2023-04-29 17:57:26 -0700

    Rendering posts

--- client/components/cards/PostList.js
+++ client/components/cards/PostList.js
@@ -1,12 +1,31 @@
+import { Avatar } from "antd";
+import renderHTML from "react-render-html";
+import moment from "moment";
+
 const PostList = ({ posts }) => {
   return (
     <>
       {posts &&
         posts.map((post) => (
           <div key={post._id} className="card mb-5">
-            <div className="card-header"></div>
-            <div className="card-body"></div>
-            <div className="card-footer"></div>
+            <div className="card-header">
+              <Avatar size={40}>{post.postedBy.name[0]}</Avatar>
+              {""}
+              <span className="pt-2 ml-3" style={{ marginLeft: "1rem" }}>
+                {post.postedBy.name}
+              </span>
+              <span className="pt-2 ml-3" style={{ marginLeft: "1rem" }}>
+                {moment(post.createdAt).fromNow()}
+              </span>
+            </div>
+            <div className="card-body">{renderHTML(post.content)}</div>
+            <div className="card-footer">
+              <img
+                src={post.image && post.image.url}
+                alt={post.postedBy.name}
+              ></img>
+              <div className="pt-3"></div>
+            </div>
           </div>
         ))}
     </>
