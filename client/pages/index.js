ea9774aafdb836bd50d4f7e58d3672019cd1a8b6
NeelakantaBhagavatula <neelakanta.bhagavatula@gmail.com>
2023-05-01 23:14:04 -0700

    Parallax image

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,20 +1,15 @@
 import { useContext } from "react";
 
 import { UserContext } from "../context";
+import ParallaxBG from "../components/cards/ParallaxBG";
 
 const Home = () => {
   const [state, setState] = useContext(UserContext);
 
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <h1 className="display-1 text-center py-5">Home Page</h1>
-          {JSON.stringify(state)}
-          <img src="/images/default.jpg" alt="image" />
-        </div>
-      </div>
-    </div>
+    <>
+      <ParallaxBG url="/images/default.jpg" />
+    </>
   );
 };
 
