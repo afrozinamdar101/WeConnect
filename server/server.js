043eef12e258886b43bb1f4ef32989e0236b75a5
NeelakantaBhagavatula <neelakanta.bhagavatula@gmail.com>
2023-05-02 01:41:12 -0700

    Socket.io server configuration

--- server/server.js
+++ server/server.js
@@ -4,11 +4,21 @@ import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import { readdirSync } from "fs";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 import authRoutes from "./routes/auth.js";
 import postRoutes from "./routes/post.js";
 
 const app = express();
+const http = createServer(app);
+const socketio = new Server(http, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-type"],
+  },
+});
 
 dotenv.config();
 
@@ -26,7 +36,7 @@ app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: [process.env.CLIENT_URL],
   })
 );
 
@@ -34,10 +44,15 @@ app.use(
 // readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 app.use("/api", [authRoutes, postRoutes]);
 
+// socket io
+socketio.on("connect", (socket) => {
+  console.log("Socket.io =>", socket.id);
+});
+
 app.post("/api/register", (req, res) => {
   console.log("Register Endpoint =>", req.body);
 });
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => console.log(`Server is runnning on port ${port}`));
+http.listen(port, () => console.log(`Server is runnning on port ${port}`));
